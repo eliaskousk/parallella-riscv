429f0fb95dc5ea3ec017161919643610e727d1eb
Elias Kouskoumvekakis <eliaskousk@gmail.com>
2016-07-14 16:59:15 +0300

    Use reserved memory for RISC-V on ZedBoard
    RISC-V on ZedBoard uses the upper 256 MB of the total 512 MB DRAM. This is now reserved in the device tree instead of U-Boot's configuration header file.

--- zedboard/uboot/zynq_zed.h
+++ zedboard/uboot/zynq_zed.h
@@ -10,16 +10,6 @@
 #ifndef __CONFIG_ZYNQ_ZED_H
 #define __CONFIG_ZYNQ_ZED_H
 
-/*
-   We specify here the SDRAM size to be used. We selected
-   256 MB from the 512 MB available on-board which means
-   the ARM cores and the RISC-V will own equal memory.
-   This is the only change needed from the default ZedBoard
-   configuration in the U-Boot tree.
-*/
-
-#define CONFIG_SYS_SDRAM_SIZE		(256 * 1024 * 1024)
-
 #define CONFIG_SYS_NO_FLASH
 
 #define CONFIG_ZYNQ_USB
