a838658222537659ee935b2a4098748d761d0afa
Elias Kouskoumvekakis <eliaskousk@gmail.com>
2016-08-09 01:16:56 +0300

    Easy change of RV64 core clock (50 or 25 MHz)

--- parallella/fpga/system_params.tcl
+++ parallella/fpga/system_params.tcl
@@ -28,3 +28,17 @@ set constraints_files [list \
                        $parallella/parallella/fpga/parallella_io.xdc \
                        $parallella/parallella/fpga/parallella_7020_io.xdc \
                       ]
+
+set rv64_clk_jitter 181.828
+set rv64_clk_phase_error 104.359
+set rv64_clk_request_freq 25.000
+set rv64_clk_multiplier 9.125
+set rv64_clk_divider1 36.500
+set rv64_clk_divider2 1
+
+#set rv64_clk_jitter 151.636
+#set rv64_clk_phase_error 98.575
+#set rv64_clk_request_freq 50.000
+#set rv64_clk_multiplier 10.000
+#set rv64_clk_divider1 20.000
+#set rv64_clk_divider2 1
